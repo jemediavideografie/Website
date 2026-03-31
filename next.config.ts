8d1eb2ea60f9ccde7be25da2c9214a85619e0dd3
jemediavideografie <je.media.videografie@gmail.com>
2026-03-31 14:30:22 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     formats: ["image/avif", "image/webp"],
+    unoptimized: true, // ← wichtig! Next.js Image Optimization läuft nicht auf static hosting
   },
-  // Allow local network testing on mobile devices in development.
-  allowedDevOrigins: ["10.42.0.38", "192.168.178.184"],
+  allowedDevOrigins: ["10.42.0.3x", "192.168.178.184"],
 };
 
 export default nextConfig;
